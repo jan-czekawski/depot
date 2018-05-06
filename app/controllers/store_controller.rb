30a6b08dc0da468b2fcd299c12cefb0667225019
Michal <bihone88@email.com>
2018-05-06 19:44:08 +0000

    Update fixtures, models and controllers.

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,7 @@
 class StoreController < ApplicationController
   def index
     @products = Product.order(:title)
+    session[:counter] += 1 unless session[:counter].nil?
+    session[:counter] = 1 if session[:counter].nil?
   end
 end
