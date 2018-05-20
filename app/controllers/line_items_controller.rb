fff7c8106cb7e46d503ad2b8d7c0da1592565afe
Michal <bihone88@email.com>
2018-05-20 01:03:46 +0000

    Move format.js from destroy to create method - fix that bug. Highlight last line_item changed to observe changes.

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -33,6 +33,7 @@ class LineItemsController < ApplicationController
     respond_to do |format|
       if @line_item.save
         format.html { redirect_to store_index_url }
+        format.js { @current_item = @line_item }
         format.json { render :show, status: :created, location: @line_item }
         session[:counter] = 0
       else
@@ -62,7 +63,6 @@ class LineItemsController < ApplicationController
     @line_item.destroy
     respond_to do |format|
       format.html { redirect_to line_items_url, notice: 'Line item was successfully destroyed.' }
-      format.js
       format.json { head :no_content }
     end
   end
